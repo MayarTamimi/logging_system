e5ffa3ea9f02a5c3a2b6498d1e5ea06570a460ef
Mayar Al-Tamimi <mayartamimi95@gmail.com>
2026-08-08 17:36:41 +0300

    feat(bulk): applying bulk insertion to postgres

--- Backend/src/worker/consumer.ts
+++ Backend/src/worker/consumer.ts
@@ -2,6 +2,9 @@ import { insertLogs } from "../modules/logs/logs.service.js";
 import { LOG_QUEUE } from "../queue/constant.js";
 import { getRabbitCHannel } from "../queue/rabbit.js";
 
+const BATCH_SIZE = 100;
+const BATCH_TIMEOUT = 1000;
+
 export async function startConsumer() {
   const channel = await getRabbitCHannel();
 
@@ -9,24 +12,59 @@ export async function startConsumer() {
     durable: true,
   });
 
-  await channel.prefetch(100);
+  await channel.prefetch(BATCH_SIZE);
 
   console.log("Worker listening...");
+
+  let messages: any[] = [];
+  let timer: NodeJS.Timeout | null = null;
+
+  async function processBatch() {
+    if (messages.length === 0) {
+      return;
+    }
+
+    const batch = messages;
+    messages = [];
+
+    if (timer) {
+      clearTimeout(timer);
+      timer = null;
+    }
+
+    try {
+      const logs = batch.map((message) =>
+        JSON.parse(message.content.toString()),
+      );
+
+      await insertLogs(logs);
+
+      for (const message of batch) {
+        channel.ack(message);
+      }
+
+      console.log(`Batch inserted: ${batch.length}`);
+
+    } catch (error) {
+      console.error("Batch processing failed:", error);
+
+      for (const message of batch) {
+        channel.nack(message, false, true);
+      }
+    }
+  }
 
   channel.consume(LOG_QUEUE, async (message) => {
     if (!message) return;
 
-    try {
-      const log = JSON.parse(message.content.toString());
+    messages.push(message);
 
-      await insertLogs([log]);
+    if (messages.length === 1) {
+      timer = setTimeout(processBatch, BATCH_TIMEOUT);
+    }
 
-      channel.ack(message);
-
-      console.log("Log inserted!");
-    } catch (err) {
-      console.error("Failed to process the log", err);
-      channel.nack(message, false, true);
+    if (messages.length >= BATCH_SIZE) {
+      await processBatch();
     }
   });
 }
